2ca6500688d0a934b45df578ff4e2cbce2c887be
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2023-12-17 16:45:36 -0800

    adding a folder that performs crud operations ussing the fs module and json file, the folder also interacts with MongoDB using the .save method to add a document to the database

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
+
 const app = require('./app');
 
 //console.log(app.get('env'));
 console.log(process.env);
 
 //create a Server
-const port = 5050;
+const port = process.env.PORT || 5050;
 
 app.listen(port, () => {
         console.log('server has started...');
